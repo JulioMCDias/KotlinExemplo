037e6cb89ae020931ce05c9eb6b24fb5fff4eb44
JulioMCDias <juliomarques11@gmail.com>
2020-03-27 20:00:48 -0300

    created project

---
+++ app/src/main/java/com/jlmcdeveloper/kotlinexemplo/login/Login.kt
@@ -0,0 +1,13 @@
+package com.jlmcdeveloper.kotlinexemplo.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jlmcdeveloper.kotlinexemplo.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
