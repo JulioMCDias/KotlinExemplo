cf50fd823726582e97c9b80620a2a93fad71def8
JulioMCDias <juliomarques11@gmail.com>
2020-04-15 20:09:37 -0300

    implemented xml new user

---
+++ app/src/main/java/com/jlmcdeveloper/kotlinexemplo/ui/newuser/NewUserActivity.kt
@@ -0,0 +1,13 @@
+package com.jlmcdeveloper.kotlinexemplo.ui.newuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jlmcdeveloper.kotlinexemplo.R
+
+class NewUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_user)
+    }
+}
